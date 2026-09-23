77c347e85a3299fd5904faf96d08e8fc389d8e1a
i-priyanshuverma <i.priyanshuverma@gmail.com>
2026-09-24 02:38:36 +0530

    fix(helpers): typehint Jev contract in jev() helper

--- src/helpers.php
+++ src/helpers.php
@@ -2,16 +2,16 @@
 
 declare(strict_types=1);
 
-use Priyanshu\LaravelJev\JevManager;
+use Priyanshu\LaravelJev\Contracts\Jev as JevContract;
 use Priyanshu\LaravelJev\Support\BatchAnalysis;
 
 if (! function_exists('jev')) {
     /**
      * Get the Jev manager or begin a batch analysis against an input string.
      */
-    function jev(?string $input = null): JevManager|BatchAnalysis
+    function jev(?string $input = null): JevContract|BatchAnalysis
     {
-        /** @var JevManager $manager */
+        /** @var JevContract $manager */
         $manager = app('jev');
 
         if ($input !== null) {
